aec357ef7b51425ec8fe500275a61f51daac42d1
Youcanprint 1 <youcanprint@boresrl.onmicrosoft.com>
2025-03-03 10:52:58 +0100

    aggiornata palette colori

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mocha: {
+          DEFAULT: '#837060',   // Base mocha color
+          light: '#A99E8A',     // Lighter variant for backgrounds or highlights
+          dark: '#584338'       // Darker variant for text or accents
+        }
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
