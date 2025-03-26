fab3fbdf70c60a47865ce0b5c928f0971945f272
Youcanprint 1 <youcanprint@boresrl.onmicrosoft.com>
2025-03-26 16:50:02 +0100

    bug fixes

--- webapp/drizzle.config.ts
+++ webapp/drizzle.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 import { config } from 'dotenv';
 
 config({ path: '.env.local' });
 
-export default defineConfig({
+export default {
   schema: './db/**/*.ts',
   out: './drizzle/migrations',
-  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    connectionString: process.env.DATABASE_URL!
   }
-});
+} satisfies Config;
